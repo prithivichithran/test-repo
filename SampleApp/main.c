57041e7da4e085e0175f6899305a531e80b10881
prithivichithran <prithivichithran@gmail.com>
2018-05-30 20:30:54 +0530

    New Addition project SampleApp

---
+++ SampleApp/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+   int a,b,c;
+   printf("Enter The Two Values to Add\n");
+   scanf("%d%d",&a,&b);
+   c=a+b;
+   printf("Added Value= %d\n",c);
+   return 0;
+}
